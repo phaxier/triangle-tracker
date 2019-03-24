83f0993d02e5a77cd810861c7adc04200c8e5e90
Noel Bor <NoelkBor@gmail.com>
2019-03-24 23:56:10 +0300

    final changes

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+    document.getElementById('result').addEventListener('click',function(){
+
+      var number1 = document.getElementById('num1').value;
+      var number2 = document.getElementById('num2').value;
+      var number3 = document.getElementById('num3').value;
+
+      alert()
+
+    })
