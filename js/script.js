a095b88fe339d6beb73e8df037e70ecacacb57f3
Noel Bor <NoelkBor@gmail.com>
2019-03-25 11:21:43 +0300

    update on the formula in the script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,27 @@
 
-    document.getElementById('result').addEventListener('click',function(){
 
-      var number1 = document.getElementById('num1').value;
-      var number2 = document.getElementById('num2').value;
-      var number3 = document.getElementById('num3').value;
+function triangleTracker(){
 
-      alert()
+  var number1 = document.getElementById('num1').value;
+  var number2 = document.getElementById('num2').value;
+  var number3 = document.getElementById('num3').value;
 
-    })
+  if (number1==0 || number2==0 || number3==0){
+    alert("Error, a triangle must have a definate length")
+  }
+
+  else if(number1+number2>number3 && number2+number3>number1 && number1+number3>number2 && number1===number2 && number2===number3 ){
+    alert('Equilateral Triangle')
+  }
+  else if(number1+number2>number3 && number2+number3>number1&&number1+number3>number2 &&number1===number2||number2===number3||number1===number3){
+    alert("Isoscelese Triangle")
+
+  }
+  else if(number1+number2>number3 && number1+number3>number2 && number1!=number2!=number3){
+  alert ("Scalene Triangle")
+  }
+  else
+  {
+    alert("Not a Triangle")
+  }
+}
